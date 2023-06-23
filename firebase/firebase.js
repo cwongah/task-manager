271ab89d399a1a6307750c89ee723594e4cd4dca
Christopher Wong <cw30355@gmail.com>
2023-06-22 21:28:40 -0400

    initial commit

---
+++ firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB2xJHOlnxseBgoXUDroNsdbuhYWpT3unI",
+  authDomain: "task-manager-4817a.firebaseapp.com",
+  projectId: "task-manager-4817a",
+  storageBucket: "task-manager-4817a.appspot.com",
+  messagingSenderId: "932546699290",
+  appId: "1:932546699290:web:0c5a89e9fe66c945c571a3",
+  measurementId: "G-H1MZGQDD06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login(){
+    return(
+        <div>
+            <button onClick={()=> console.log('login click')} >
+                Login
+            </button>
+        </div>
+    )
+}
+
+export default Login
